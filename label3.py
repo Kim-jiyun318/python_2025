8dcd91637c1752dfa22951dc6b676422cdc8a947
Kim-jiyun318 <jiyunkim3607@gmail.com>
2025-10-01 01:31:07 +0900

    두번째커밋

--- label3.py
+++ label3.py
@@ -1,7 +1,7 @@
 from tkinter import*
 
 root = Tk()
-photo = PhotoImage(file="cat.jpeg")
+photo = PhotoImage(file="\Python 2025\Twitter.png")
 label = Label(root, image=photo)
 label.pack()
 root.mainloop()
