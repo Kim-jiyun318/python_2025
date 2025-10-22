9504a3b3b05addee49f724091b42c3a382a2dae5
Kim-jiyun <jiyunkim3607@gmail.com>
2025-10-23 02:53:59 +0900

    Add files via upload

---
+++ 9장 연습문제/study9-4.py
@@ -0,0 +1,20 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name #직원의 이름과 연봉을 저장하고!
+        self.salary = salary
+    
+    def usual_salary(self):
+        print(f"{self.name}의 연봉은 {self.salary}입니다.")
+
+    def raise_salary(self):
+        self.salary += 2000
+        print(f"{self.name}의 연봉이 {self.salary}으로 증가되었습니다.")
+    
+employee1 = Employee("Kim", 5000)
+employee2 = Employee("Lee", 6000)
+
+employee1.usual_salary()
+employee2.usual_salary() #무조건 안에다가 만들 필요는 없구나
+                        #조건에 없었으니까 다음에는 usual_salary를 여기에 출력하자.
+employee1.raise_salary()
+employee2.raise_salary()
---
+++ 9장 연습문제/study9-6.py
@@ -0,0 +1,27 @@
+class Bank:
+    #balance = 1000
+    insert_rate= 0.3 #예금이자
+    def __init__(self, name, number, balance):
+        self.name = name
+        self.number = number
+        self.balance = balance #각자의 계좌니까 인스턴스변수
+
+    def deposit(self, money):
+        self.balance += money
+        print(f"저축 후 잔고: {self.balance}")
+    
+    def withdraw(self, money):
+        if self.balance < money:
+            print("잔액 부족")
+            return None
+        self.balance -= money
+        print("인출 성공")
+        print(f"인출 후 잔고: {self.balance}")
+
+bank = Bank("Kim", "0000", 1000) #클래스 변수를 지정하라는 걸 잘못 이해했다.
+
+print(f"초기 잔고: {bank.balance}")
+
+bank.deposit(500)
+bank.withdraw(200)
+bank.withdraw(1400)
