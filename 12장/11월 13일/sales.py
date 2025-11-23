783cf25746a7a93f53312c3f8a01a38698ee872d
Kim-jiyun318 <jiyunkim3607@gmail.com>
2025-11-23 22:39:57 +0900

    11월 13일 커밋

--- 12장/11월 13일/sales.py
+++ 12장/11월 13일/sales.py
@@ -10,6 +10,7 @@ sum = 0
 count = 0
 
 #입력과 출력을 위한 파일을 열고 작업을 수행한다.
+# 읽기, 쓰기 둘다 할 것이므로 각각 써준다.
 with open(infilename, "r", encoding="utf-8") as infile, open(outfilename, "w", encoding="utf-8") as outfile:
     #입력 파일에서 한 줄을 읽어서 합계를 계산한다.
     for line in infile:
@@ -18,5 +19,6 @@ with open(infilename, "r", encoding="utf-8") as infile, open(outfilename, "w", e
         count += 1
     
     #총매출과 일평균 매출을 출력 파일에 기록한다.
+    #with 블럭이 종료되면 자동으로 파일을 닫으므로 작성(w)까지도(!) with 블럭 내에서 해야 한다.
     outfile.write("총매출 = " + str(sum) + "\n")
     outfile.write("평균 일매출 = " + str(sum // count))
